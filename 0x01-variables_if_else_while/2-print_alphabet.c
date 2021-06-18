354a022942678fdaae89ded02774e9ad23c08b41
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-18 10:13:47 +0000

    Add a C program to display the alphabets
    This program will show lowercase english alphabets one after the other
    on a single line. It ends with a new line and returns 0.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
