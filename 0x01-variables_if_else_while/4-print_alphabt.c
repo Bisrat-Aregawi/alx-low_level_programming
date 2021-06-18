e58544082d85f68f10936d5f9f55867a8ed9c81e
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-18 10:45:14 +0000

    Add a script to display selected alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+}
