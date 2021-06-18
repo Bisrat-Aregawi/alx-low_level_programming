aeb568c847fd50f6348dbf721bcbebdf2aed55ca
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-18 11:14:55 +0000

    Add a C program to display reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	
+	return (0);
+}
