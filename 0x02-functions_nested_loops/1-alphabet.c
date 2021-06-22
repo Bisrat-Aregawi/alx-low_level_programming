d2c587ebae6bce4179c7b61d8777544e623c91ec
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-22 12:53:13 +0000

    Fix error on 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,31 +1,11 @@
-#include <stdio.h>
-/**
- * print_alphabet - Function that prints alphabets
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	int i;
+#include "holberton.h"
 
-	i = 0;
-
-	while (i <= 25)
-	{
-		putchar(i + 97);
-		i++;
-	}
-	putchar('\n');
-}
 /**
- * main - Function that first executes
+ * main- Function that first executes
  *
  * Return: 0
  */
 int main(void)
 {
-	print_alphabet();
 	return (0);
 }
-
-void print_alphabet(void);
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void writeHolberton(void)
 {
 	printf("Holberton\n");
+}
+void print_alphabet(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 25)
+	{
+		_putchar(i + 97);
+		i++;
+	}
+	_putchar('\n');
 }
