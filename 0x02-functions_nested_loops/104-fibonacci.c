6cf4e131cd60da2f25400247888e6d4f46f9dfe9
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-23 21:41:21 +0000

    A C program to list first 98 fibonaccis

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 50 fibonaccis
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long n1, n2, n3;
+
+	n1 = 1;
+	n2 = 1;
+	n3 = 2;
+
+	for (i = 0; i < 98; i++)
+	{
+		if (i <= 0)
+		{
+			printf("%d, ", 1);
+		} else if (i != 98)
+		{
+			printf("%ld, ", n3);
+			n1 = n2;
+			n2 = n3;
+			n3 = n1 + n2;
+		} else
+		{
+			printf("%ld\n", n3);
+		}
+	}
+	return (0);
+}
