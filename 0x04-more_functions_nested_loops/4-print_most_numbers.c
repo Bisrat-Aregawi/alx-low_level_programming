d981fc870eed4e497629fcb2d6b6c3317a95f16a
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-24 08:47:54 +0000

    Add a C program to list most numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Function that prints from 0 to 9
+ * but skips 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
