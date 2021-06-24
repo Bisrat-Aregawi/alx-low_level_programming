05465fc9a03f5ca1a5dab8a8fc95fc4fa5981ee6
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-24 08:33:10 +0000

    Add a C program to list from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Function prints from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
