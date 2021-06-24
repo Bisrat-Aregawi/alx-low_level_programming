529bfb35c609bad4e1de6ad8b360a61c8a36130f
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-24 01:14:44 +0000

    Add 0-main.c to check for printing of 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - tests a function if it returns 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative();
+
+#endif /* HOLBERTON-H */
