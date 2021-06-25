7a8cd2d6203bbe3fc671d563dd222fa302d43e86
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-25 09:40:06 +0000

    Add a C program to make a \ incline

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Function that prints \ as desired
+ * @n: number of times \ should be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
