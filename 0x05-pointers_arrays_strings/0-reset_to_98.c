001ada9e51ccab5e675ede4ca7919b3c2be11f26
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-06-28 10:59:58 +0000

    Add a C program to update variables with pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Function takes a pointer as argument and updates
+ * the value it points to.
+ * @n: pointer passed to reset_to_98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
