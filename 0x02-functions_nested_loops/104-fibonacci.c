651974e6ef9479fe8e71daf62acd5b3c2e60c348
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-07-09 03:59:36 +0000

    Add a script to create a static library

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 
+#define NUM_SPLITER 10000000000
+#define SPLITER_LIMIT 9999999999
+
 /**
- * main - Prints 50 fibonaccis
+ * main - Function lists first 98 FIBONACCIS
  *
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
-	unsigned long n1, n2, n3;
+	unsigned long n1, n2, fib;
+	unsigned long n11, n12, n21, n22, H1, H2;
 
-	n1 = 1;
-	n2 = 1;
-	n3 = 2;
+	n1 = 0, n2 = 1;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
-		if (i <= 0)
+		fib = n1 + n2;
+		printf("%lu, ", fib);
+
+		n1 = n2;
+		n2 = fib;
+	}
+
+	n11 = n1 / NUM_SPLITER;
+	n12 = n1 % NUM_SPLITER;
+	n21 = n2 / NUM_SPLITER;
+	n22 = n2 % NUM_SPLITER;
+
+	for (i = 0; i < 6; i++)
+	{
+		H1 = n11 + n21;
+		H2 = n12 + n22;
+		if (n12 + n22 > SPLITER_LIMIT)
 		{
-			printf("%d, ", 1);
-		} else if (i != 98)
-		{
-			printf("%ld, ", n3);
-			n1 = n2;
-			n2 = n3;
-			n3 = n1 + n2;
-		} else
-		{
-			printf("%ld\n", n3);
+			H1 += 1;
+			H2 %= NUM_SPLITER;
 		}
+		if (i < 5)
+			printf("%lu%lu, ", H1, H2);
+		else
+			printf("%lu%lu\n", H1, H2);
+
+		n11 = n21;
+		n12 = n22;
+		n21 = H1;
+		n22 = H2;
 	}
 	return (0);
 }
