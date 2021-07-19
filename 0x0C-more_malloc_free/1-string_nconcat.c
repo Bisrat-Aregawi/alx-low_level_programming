306c8fee2d098b791b857e7240aa278c72b91774
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-07-19 01:29:05 +0000

    Commit unfinished 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,30 +12,96 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i, j, totlen;
 	char *str;
+	int nullnessRes = nullnessChk(s1, s2);
 
 	i = 0, j = 0;
 
-	totlen = (n >= strlen(s2)) ? strlen(s1) + strlen(s2) : strlen(s1) + n;
-
-	str = malloc(sizeof(char) * totlen + 1);
-	if (str == NULL)
-		return (NULL);
-
-	while (i < totlen)
+	if (s1 == NULL && s2 != NULL)
 	{
-		if (*(s1 + i))
-			*(str + i) = *(s1 + i);
+		if (n >= strlen(s2))
+			totlen = strlen(s2);
 		else
-			break;
-		i++;
-	}
-	while (i < totlen)
+			totlen = n;
+
+		str = malloc(sizeof(char) * totlen + 1);
+
+		if (str == NULL)
+			return (NULL);
+
+		while (i < totlen)
+		{
+			if (*(s1 + i))
+				*(str + i) = *(s2 + i);
+			else
+				break;
+			i++;
+		}
+	} else if (s1 != NULL && s2 == NULL)
 	{
-		*(str + i) = *(s2 + j);
-		i++;
-		j++;
-	}
+		totlen = strlen(s1);
+
+		str = malloc(sizeof(char) * totlen + 1);
+
+		if (str == NULL)
+			return (NULL);
+
+		while (*s1)
+		{
+			*s1 = *str;
+			s1++;
+			str++;
+		}
+	} else if ( s1 != NULL && s2 != NULL)
+	{
+		totlen = (n >= strlen(s2)) ? strlen(s1) + strlen(s2) : strlen(s1) + n;
+
+		str = malloc(sizeof(char) * totlen + 1);
+
+		if (str == NULL)
+			return (NULL);
+
+		while (i < totlen)
+		{
+			if (*(s1 + i))
+				*(str + i) = *(s1 + i);
+			else
+				break;
+			i++;
+		}
+		while (i < totlen)
+		{
+			*(str + i) = *(s2 + j);
+			i++;
+			j++;
+		}
+
+	} else
+		return (NULL);
 	*(str + i) = '\0';
 
 	return (str);
+}
+
+int nullnessChk (char *s1, char *s2)
+{
+	if (s1 == NULL && s2 != NULL)
+		return (1);
+	else if (s1 != NULL && s2 == NULL)
+		return (2);
+	else if (s1 == NULL && s2 == NULL)
+		return (0);
+	return (3);
+}
+
+void concatenator (char *s, char *str)
+{
+	while (1)
+	{
+		if (*s)
+			*(str) = *s;
+		else
+			break;
+		str++;
+		s++;
+	}
 }
