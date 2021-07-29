be4af632463be47eec7e69bdf0c8cc4f80c71f0f
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-07-29 01:46:31 +0000

    Add a header file to define a structure 'dog'
    This is for task 0 of 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog characteristics
+ * @name: Name of a dog
+ * @age: Age of a dog
+ * @owner: Owner of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
