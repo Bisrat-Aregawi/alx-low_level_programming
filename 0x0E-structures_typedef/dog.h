0d68d2d4f12f42caf3aef1bdeb770b56ffdf24a1
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-07-29 02:12:35 +0000

    Add C Program to show initialized contents of struct
    This is for task 2 of 0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+#include <stddef.h>
+#include <stdio.h>
 /**
  * struct dog - Dog characteristics
  * @name: Name of a dog
@@ -14,5 +16,6 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* _DOG_H_ */
