13261012415bac1b4760e715f87c352768eae0aa
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-07-29 03:24:09 +0000

    Modify 'dog.h' to define 'dog_t' type
    This is for task 3 of 0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @age: Age of a dog
  * @owner: Owner of a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
