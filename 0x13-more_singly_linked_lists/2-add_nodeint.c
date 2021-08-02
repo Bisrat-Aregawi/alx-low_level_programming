6df496d148f61b0c9406788cba2cf73a42077b79
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-08-02 10:08:54 +0000

    Add C program to add nodes to beginning of a list
    Program is solution to task 2 of 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Function addes new Nodes at the begining and
+ * contents of those nodes are passed along with pointer to the head node
+ * @head: Pointer to pointer to first Node in the list
+ * @n: Content of new node of type 'const int'
+ *
+ * Return: Address of new node created, NULL if malloc fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = *head;
+
+	*head = newNode;
+
+	return (newNode);
+}
