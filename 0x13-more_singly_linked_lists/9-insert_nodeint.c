482828c9690070c59f94f8df19348be7fbc288d2
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-08-03 15:34:34 +0000

    Fix error on '9-insert_nodeint.c
    Fix includes handling of invalid indices & handling insertion at the
    position of first node.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,44 +1,48 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - Function inserts nodes at given index
  * @head: Pointer to pointer of first node
  * @idx: Index to insert newly created node
  * @n: Content of newly created node
- *
  * Return: Address of newly created node, NULL if allocation fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	/* Initialize a local index for comparison */
+	/* Initialize local index to keep track of traversal */
 	unsigned int local_idx = 0;
-	/* Initialize a copy of head to store its address for traversal */
-	listint_t *local_head;
-	/* Initialize node to hold new contents and be positioned at 'idx' */
+	/* Declare traverser node for looping purposes */
+	listint_t *traverser;
+	/* Declare new node to be inserted at 'idx' */
 	listint_t *newNode;
-
-	/* Copy address of first node to 'local_head' */
-	local_head = *head;
-	/* Allocate and check memory for 'newNode' */
-	newNode = malloc(sizeof(listint_t));
-	if (newNode == NULL)
-		return (NULL);
-
-	/* Insert new values to 'newNode' */
-	newNode->n = n;
-	/* Traverse list until the point where we insert 'newNode' */
-	while (local_head != NULL)
+	/* Initialize 'traverser' to the begining of list */
+	traverser = *head;
+	/* Iterate through list and stop at one before 'idx' */
+	while (traverser != NULL)
 	{
-		if (local_idx != (idx - 1))
-			local_head = local_head->next, local_idx++;
+		/* If at one before 'idx', break loop, if not traverse */
+		if (local_idx < (idx - 1))
+		{
+			traverser = traverser->next;
+			local_idx++;
+		}
 		else
 			break;
 	}
-	/* Have 'newNode' point to (local_idx + 1) indexed node */
-	newNode->next = local_head->next;
-	/* Have (local_idx) indexed node point to 'newNode' */
-	local_head->next = newNode;
-
-	/* Return new node inserted */
+	/* If reached end of list but still idx is big?, return NULL */
+	if (traverser == NULL && local_idx < idx)
+		return (NULL);
+	/* Allocate and check memory for newNode */
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	/* Assign data to data section of the node */
+	newNode->n = n;
+	/* If asked to insert at the beginning manipulate '*head' */
+	if (idx == 0)
+		newNode->next = *head, *head = newNode;
+	/* If anywhere other than beginning manipulate 'traverser' */
+	else
+		newNode->next = traverser->next, traverser->next = newNode;
+	/* Return newly created node */
 	return (newNode);
 }
